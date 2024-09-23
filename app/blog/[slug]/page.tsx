cd7a30debac056042cafeabd57ff19d74500eb60
David Murdoch <dsmurdoch357@gmail.com>
2024-09-23 18:46:56 +0100

    chore(prettier): correcting prettier mistake

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
-import { CustomMDX } from "app/components/mdx";
 import { formatDate, getBlogPosts } from "app/blog/utils";
+import { CustomMDX } from "app/components/mdx";
 import { baseUrl } from "app/sitemap";
 
 export async function generateStaticParams() {
@@ -82,10 +82,10 @@ export default function Blog({ params }) {
           }),
         }}
       />
-      <h1 className="title font-semibold text-2xl tracking-tighter">
+      <h1 className="title text-2xl font-semibold tracking-tighter">
         {post.metadata.title}
       </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+      <div className="mt-2 mb-8 flex items-center justify-between text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt)}
         </p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import "./global.css";
+
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import Footer from "./components/footer";
+import { Navbar } from "./components/nav";
 import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
@@ -47,13 +48,13 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
+        "bg-white text-black dark:bg-black dark:text-white",
         GeistSans.variable,
         GeistMono.variable,
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="mx-4 mt-8 max-w-xl antialiased lg:mx-auto">
+        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
           <Navbar />
           {children}
           <Footer />
